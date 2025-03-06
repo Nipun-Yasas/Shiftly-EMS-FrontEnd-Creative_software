457c1dd9e0c8acb9288f0c235265df164cc25dce
Devdini Senevirathna <devdinichaniya2001@gmail.com>
2025-03-06 17:46:17 +0530

    add tailwind

---
+++ FrontEnd/ems-frontend/src/app/(employee)/(profile)/basicinfo/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+        <div className="container w-auto mt-30 ml-40 pt-5 pb-5 pl-20 shadow-md bg-white rounded-xl">
+       <div className="text-4xl text-red-600 font-bold ">Profile</div>
+       <div className="sub-path">
+        <span className="text-gray-400 mx-2 ">Employee <span>›</span> </span> 
+        <span className="text-gray-500 mx-2 "> profile <span>›</span></span>
+        </div>
+
+    </div>
+
+    <div className="profile-basic shadow-md  bg-white rounded-2xl">
+           
+    </div>
+
+    </>
+  )
+}
+
+export default page
