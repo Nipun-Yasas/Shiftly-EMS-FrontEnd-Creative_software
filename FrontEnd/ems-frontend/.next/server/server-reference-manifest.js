809b1132c06d8a815e0f78dfd49047df47312ad1
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-03-10 18:10:09 +0530

    configured with dependancies

---
+++ FrontEnd/ems-frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"d6Rr0kd6sX0KnYyOcSjK5hQxQrxC1yk96A1y0KUcluE=\"\n}"
---
+++ FrontEnd/ems-frontend/.next/static/chunks/src_app_page_ee2e82d4.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_ee2e82d4.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_ee2e82d4.js",
+  "chunks": [
+    "static/chunks/node_modules_67f28dce._.js"
+  ],
+  "source": "dynamic"
+});
